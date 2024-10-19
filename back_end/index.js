d8b4c03dc39c1f6f5a804aca0f7730593bad7bd8
Lubasi Milupi <lubasimilupi@gmail.com>
2024-10-19 17:31:19 +0200

    user routes

--- back_end/index.js
+++ back_end/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import cors from "cors";
+import userRoute from "./Routes/Users";
+
+const corsOptions = {
+  origin: ["'http://localhost:5173'"],
+};
 
 const app = express();
+
+app.use(cors(corsOptions));
+app.use("user", userRoute);
 
 const PORT = process.env.PORT || 3000;
 
