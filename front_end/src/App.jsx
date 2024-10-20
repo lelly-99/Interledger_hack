c4f9c008f507e7e9a29a14be1af70b69d1c74214
Lubasi Milupi <lubasimilupi@gmail.com>
2024-10-20 05:41:55 +0200

    Card file

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -3,8 +3,6 @@ import Navbar from "./components/Navbar";
 import DashBoard from "./components/DashBoard";
 import Summary from "./components/Summary";
 
-import "./App.css";
-
 function App() {
   return (
     <div className="grid-container">
