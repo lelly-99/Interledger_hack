8b15b04ee652ef93034d6d0003ee25454fe65dd1
Lubasi Milupi <lubasimilupi@gmail.com>
2024-10-20 06:05:20 +0200

    card showing amount

--- front_end/src/components/Item.jsx
+++ front_end/src/components/Item.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Item() {
-  return <div className="item"></div>;
+  return (
+    <div className="item ">
+      <div>
+        <h3>ZAR</h3>
+      </div>
+      <div>
+        <h4>Current Balance:</h4>
+      </div>
+      <div>
+        <h1>20000</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Item;
